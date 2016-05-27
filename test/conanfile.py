61c543a222c3c6424cdaa1683cfd851eec158086
Rui Pires <rui@sennin.pt>
2016-05-27 15:40:09 +0000

    add support for conan test

---
+++ test/conanfile.py
@@ -0,0 +1,31 @@
+from conans import ConanFile
+from conans import CMake
+import os
+
+############### CONFIGURE THESE VALUES ##################
+default_user = "rui"
+default_channel = "testing"
+#########################################################
+
+channel = os.getenv("CONAN_CHANNEL", default_channel)
+username = os.getenv("CONAN_USERNAME", default_user)
+
+class DefaultNameConan(ConanFile):
+    name = "DefaultName"
+    version = "0.1"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "Gumbo/0.10.1@%s/%s" % (username, channel)
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self.settings)
+        self.run('cmake %s %s' % (self.conanfile_directory, cmake.command_line))
+        self.run("cmake --build . %s" % cmake.build_config)
+
+    def imports(self):
+        self.copy(pattern="*.dll", dst="bin", src="bin")
+        self.copy(pattern="*.dylib", dst="bin", src="lib")
+
+    def test(self):
+        self.run("cd bin && .%sgumbotest" % (os.sep))
+
