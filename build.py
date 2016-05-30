ed2af4bb81112ab47ee4ac11b78ca7b8b66032b4
Rui Pires <rui@sennin.pt>
2016-05-30 00:12:00 +0000

    adds workaround include dir for visual studio

--- build.py
+++ build.py
@@ -2,5 +2,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds(shared_option_name="Gumbo:shared", pure_c=False) # pure_c=False needed for Visual Studio support
+    builder.add_common_builds(shared_option_name="Gumbo:shared", pure_c=True)
     builder.run()
